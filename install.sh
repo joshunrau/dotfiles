c3a824ea902b1bf6c237967fdc4273ca409ce29d
joshunrau <contact@joshuaunrau.com>
2024-01-28 11:07:02 -0500

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ set -eu -o pipefail
 
 TARGET_DIR=~/.dotfiles
 if [ ! -d "$TARGET_DIR" ]; then
-  git clone https://github.com/joshunrau/dotfiles.git "$TARGET_DIR"
+  git clone https://github.com/joshunrau/dotfiles.git "$TARGET_DIR" && cd "$TARGET_DIR"
+else
+  cd "$TARGET_DIR" && git pull
 fi
 
 cd "$TARGET_DIR" && python dotfiles --help
