371b7a3c9918824e965aacbb3c63247627324e93
joshunrau <contact@joshuaunrau.com>
2024-06-24 11:57:41 -0400

    update dotfiles

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,17 +7,16 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
-require 'options'
+require('core.options')
+
+require('core.keymaps')
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
-
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require('lazy-bootstrap')
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require('lazy-plugins')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
