c7d7877a1ca95752137fe8750f3755a3cf5345b4
joshunrau <contact@joshuaunrau.com>
2024-06-24 18:37:07 -0400

    move out plenary plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,6 @@ return {
     event = 'VimEnter',
     branch = '0.1.x',
     dependencies = {
-      'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
 
