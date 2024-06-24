850ff71fb95dc7b00c9347d3d2104a60598565ea
joshunrau <contact@joshuaunrau.com>
2024-06-24 18:52:59 -0400

    update neovim theme

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,17 @@
 return {
   {
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme('tokyonight-night')
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi('Comment gui=none')
+    priority = 1000,
+    config = function()
+      require('tokyonight').setup({
+        style = 'night',
+        transparent = true,
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+      })
+      vim.cmd('colorscheme tokyonight')
     end,
   },
 }
