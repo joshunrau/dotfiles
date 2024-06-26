32601519ea7d43c0d0100f9f83f790fbaab75815
joshunrau <contact@joshuaunrau.com>
2024-06-26 14:15:59 -0400

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,8 +1,8 @@
--- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Set "\" as the leader key
+vim.g.mapleader = '\\'
+vim.g.maplocalleader = '\\'
 
 require('config.options')
 require('config.keymaps')
 require('config.autocmds')
-require("config.lazy")
+require('config.lazy')
