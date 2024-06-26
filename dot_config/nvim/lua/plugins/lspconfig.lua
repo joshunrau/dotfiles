128bb6bdd09a07c5b4c87bfe067125e1e4acccfa
joshunrau <contact@joshuaunrau.com>
2024-06-26 13:54:35 -0400

    update

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -156,33 +156,18 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
+        bashls = {},
+        cssls = {},
+        html = {},
+        jsonls = {},
+        lua_ls = {},
+        marksman = {},
+        mdx_analyzer = {},
+        pyright = {},
+        ruff = {},
+        rust_analyzer = {},
+        tailwindcss = {},
+        tsserver = {},
       }
 
       -- Ensure the servers and tools above are installed
